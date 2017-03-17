b6365fa642f7ae99186ba75372ddb8efa1f86bda
lsolorzano <lrsolorzano@gmail.com>
2017-03-17 18:41:59 -0400

    Version 0.01

---
+++ src/main/java/BDataP1/TwitterKeyWorkReducer.java
@@ -0,0 +1,25 @@
+package BDataP1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Created by lsolorzano on 3/17/2017.
+ */
+
+public class TwitterKeyWorkReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        int count = 0;
+
+        for (IntWritable value : values){
+            count += value.get();
+        }
+        context.write(key, new IntWritable(count));
+    }
+}
